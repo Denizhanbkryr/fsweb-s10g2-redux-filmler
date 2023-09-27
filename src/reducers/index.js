3bb3965db7f0d0bd0ce881a4d50eeee2123da658
Denizhan Bakıryor <denizhanbakiryor@gmail.com>
2023-09-27 10:47:35 +0300

    S10G2 Projesi çözüldü.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import favoriteReducer from "./favouriteReducer";
+import movieReducer from "./movieReducer";
 
-import movieReducer from './movieReducer';
+const rootReducer = combineReducers({
+  movieReducer,
+  favoriteReducer,
+});
 
-export default movieReducer;
+export default rootReducer;
